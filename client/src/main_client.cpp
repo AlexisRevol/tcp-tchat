410c8fee4f68bbca545ae1d662988ba0fbb4d6b4
AlexisRevol <revol.alexis.lgm@gmail.com>
2025-06-15 14:54:29 +0200

    init:template of app

---
+++ client/src/main_client.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Lancement du client TCP-Tchat..." << std::endl;
+
+    // TODO:
+    // 1. Créer un socket (socket())
+    // 2. Se connecter au serveur (connect())
+    // 3. Créer un thread pour écouter les messages du serveur
+    // 4. Dans la boucle principale, lire l'entrée de l'utilisateur et l'envoyer au serveur
+
+    std::cout << "Client deconnecte." << std::endl;
+    return 0;
+}
---
+++ server/src/main_server.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Lancement du serveur TCP-Tchat..." << std::endl;
+
+    // TODO:
+    // 1. Créer un socket (socket())
+    // 2. Lier le socket à une adresse IP et un port (bind())
+    // 3. Mettre le socket en écoute (listen())
+    // 4. Accepter les nouvelles connexions dans une boucle (accept())
+    // 5. Pour chaque client, créer un thread pour gérer la communication
+
+    std::cout << "Serveur arrete." << std::endl;
+    return 0;
+}
